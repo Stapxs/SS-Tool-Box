2de94ca9dc6e8936b9a37a1950afe393fd3915d7
Stapxs <42486439+Stapxs@users.noreply.github.com>
2020-01-08 21:05:48 +0800

    完善配色（Main - UI）
    完善了龙猫蓝的配图

--- SS Tool Box/obj/Debug/SS Tool Box_Content.g.i.cs
+++ SS Tool Box/obj/Debug/SS Tool Box_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resource/bgfortheams/blcorangeds.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resource/bgfortheams/ltcatblues.png")]
 
 
